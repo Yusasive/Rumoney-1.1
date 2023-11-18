78c0a6032f129a112d848bb5a09abf5e0c998be0
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2023-11-18 22:57:12 +0100

    carousel data

--- src/component/data.js
+++ src/component/data.js
@@ -84,4 +84,20 @@ export const productData = [
     description:
       "Rumoney is a life saver. I have been exposed to a lot in the period of taking the course",
   },
+  {
+    id: 9,
+    imageurl:
+      "https://images.unsplash.com/photo-1625772452859-1c03d5bf1137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "John Ajayi",
+    description:
+      "Rumoney is a life saver. I have been exposed to a lot in the period of taking the course",
+  },
+  {
+    id: 10,
+    imageurl:
+      "https://images.unsplash.com/photo-1625772452859-1c03d5bf1137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "John Ajayi",
+    description:
+      "Rumoney is a life saver. I have been exposed to a lot in the period of taking the course",
+  },
 ];
